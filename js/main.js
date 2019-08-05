1d4281de9db2745ee9bb0dcd05e5c450a87134f3
maxbez <rebeksa556@gmail.com>
2019-08-05 17:25:39 +0300

    show

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(function () {
+	$('.menu__links-item').click(function () {
+		$(this).toggleClass('active');
+	});
+});
+
+
+(function($){
+  $(function() {
+    $('.menu__icon').on('click', function() {
+      $(this).closest('.menu')
+        .toggleClass('menu_state_open');
+    });
+    
+    $('.menu__links-item').on('click', function() {
+      // do something
+
+      $(this).closest('.menu')
+        .removeClass('menu_state_open');
+    });
+  });
+})(jQuery);
